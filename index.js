8c9fd2cb6a2bdaf85bcfbffd0cbb34a933d83dfc
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-03-12 21:34:17 +0100

    scrollbar effect

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+
+
+let lastScroll = 0 ; 
+
+window.addEventListener('scroll' , () => {
+    if (window.scrollY < lastScroll) {
+        navbar.style.top = 0;
+        // console.log(navbar);
+        }
+    else {
+        navbar.style.top = "-60px";
+    }
+
+    lastScroll = window.scrollY;
+    // console.log(lastScroll);
+})
+
+
+
